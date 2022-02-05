a8ca1234590a2cb2c5b9132e2513bf81ae85ae10
bobyrev-ilya <bobyrev-1997@mail.ru>
2022-02-05 22:37:35 +0300

    hui

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 console.log("TEST")
-//var hero = new Model("test", "https://images-ext-1.discordapp.net/external/7GS47PUyt-1Bv6xa5Hy5QtQQBO3B84uXZwelmcJMewk/https/static.wikia.nocookie.net/overwatch_gamepedia/images/c/c5/Torbjorn-portrait.png");
 var hero = new Model("test", "resources/torb.png");
 hero.drawModel();
 console.log(hero);
-=======
-console.log("FKKFKFK");
-console.log("push master");
->>>>>>> origin/master
+
