d3d836c0b2447a133fe6d9be91385e308e1d9458
bobyrev-ilya <bobyrev-1997@mail.ru>
2022-02-05 22:31:34 +0300

    add hero model

--- scripts/test.js
+++ scripts/test.js
@@ -1,2 +1,5 @@
-console.log("FKKFKFK");
-console.log("qweqwe");
+console.log("TEST")
+//var hero = new Model("test", "https://images-ext-1.discordapp.net/external/7GS47PUyt-1Bv6xa5Hy5QtQQBO3B84uXZwelmcJMewk/https/static.wikia.nocookie.net/overwatch_gamepedia/images/c/c5/Torbjorn-portrait.png");
+var hero = new Model("test", "resources/torb.png");
+hero.drawModel();
+console.log(hero);
