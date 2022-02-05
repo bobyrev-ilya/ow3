9b606cf9c9b407e3d402f0aa1864c14e69f518c0
VityaYarosh <vitya.yarosh.1997@gmail.com>
2022-02-05 20:23:11 +0300

    add style

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,6 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+ctx.fillRect(0, 0, 1000, 1000);
+ctx
+
