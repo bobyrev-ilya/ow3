b70a089e85402fb15156c6daf9a89c72327f053f
Andrey <andrey.201200@gmail.com>
2022-02-06 10:35:08 +0300

    Немного рефакторинга, конфиг, задел для менюшки

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,9 @@
 //настройки канваса
 var canvas = document.getElementById("canvas");
-canvas.width = 1280;
-canvas.height = 720;
+canvas.width = ow3params.viewportWidth;
+canvas.height = ow3params.viewportHeight;
 
-//иницализация контекста
+//инициализация контекста
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "silver";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//параметры игры
-var HERO_WIDTH = 200;
-var HERO_HEIGHT = 200;
-
-
-//утилиты для игры
-//Возвращает картинку после ее полной загрузки (Promise)
-var imageLoader = (key, resourcePath) => {
-    return new Promise((resolve, reject) => {
-        var image = new Image();
-        image.src = resourcePath;
-        image.key = key;
-        image.onload = () => resolve(image);
-    })
-}
-
-var loadResourcesAndStartGame = (imageLoaderList, startCallback) => {
-    Promise.all(imageLoaderList)
-        .then(images => {
-                imageStore = new Map(images.map(image => [image.key, image]));
-                startCallback();
-    });
-}
